ed1e4ccc2c42c1c7295543274ef9f438aa87b6e3
jade <jade@hopepunk.me>
2025-06-13 18:38:56 +1000

    loads mantine

--- src/root.tsx
+++ src/root.tsx
@@ -1,17 +1,21 @@
+import "@mantine/core/styles.css";
+
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
+import { ColorSchemeScript, MantineProvider, mantineHtmlProps } from "@mantine/core";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" {...mantineHtmlProps}>
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>My App</title>
+        <ColorSchemeScript />
         <Meta />
         <Links />
       </head>
       <body>
-        {children}
+        <MantineProvider defaultColorScheme="dark">{children}</MantineProvider>
         <ScrollRestoration />
         <Scripts />
       </body>
