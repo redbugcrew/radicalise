ca0c19d1e8887e7780da9e23eb8f1a0ca66c4bef
jade <jade@hopepunk.me>
2025-06-18 16:13:39 +1000

    format interval dates

--- frontend/src/components/IntervalSelector.tsx
+++ frontend/src/components/IntervalSelector.tsx
@@ -1,13 +1,25 @@
 import { Group, ActionIcon, Text, Stack } from "@mantine/core";
 import { IconChevronLeft, IconChevronRight } from "@tabler/icons-react";
 import type { DateInterval } from "../store/intervals";
+import type { DateOnly } from "@urbdyn/date-only";
 
 interface IntervalSelectorProps {
   intervals: DateInterval[];
   current_interval: DateInterval | null;
 }
 
-export default function IntervalSelector(props: IntervalSelectorProps) {
+function DateText({ date }: { date: DateOnly }) {
+  const dateTime = new Date(date.year, date.month, date.day);
+  const options: Intl.DateTimeFormatOptions = { day: "numeric", month: "long" };
+  const formattedDate = dateTime.toLocaleDateString("en-US", options);
+  return <Text span>{formattedDate}</Text>;
+}
+
+export default function IntervalSelector({ intervals, current_interval }: IntervalSelectorProps) {
+  if (!current_interval) {
+    return null;
+  }
+
   return (
     <Group justify="space-between">
       <ActionIcon variant="default" aria-label="Previous Page" size="lg">
@@ -15,10 +27,10 @@ export default function IntervalSelector(props: IntervalSelectorProps) {
       </ActionIcon>
       <Stack align="center" justify="center" gap={0}>
         <Text span fw="bold" size="lg">
-          Interval 10
+          Interval {current_interval.id}
         </Text>
         <Text span c="dimmed">
-          12 June - 7th July
+          <DateText date={current_interval.start_date} /> - <DateText date={current_interval.end_date} />
         </Text>
       </Stack>
       <ActionIcon variant="default" aria-label="Next Page" size="lg">
