e61e15cff578e0c6a0c2d3bb57c8f851ed6e8180
jade <jade@hopepunk.me>
2025-06-22 12:39:24 +1000

    moved routes into auth_routes

--- backend/src/auth/mod.rs
+++ backend/src/auth/mod.rs
@@ -1,62 +1,8 @@
-use axum::{
-    Extension,
-    http::{Response, StatusCode},
-    response::IntoResponse,
-};
-use serde::Deserialize;
-use sqlx::SqlitePool;
-use utoipa::ToSchema;
-use utoipa_axum::{router::OpenApiRouter, routes};
-
-use crate::auth::auth_repo::{AuthRepo, AuthRepoError};
+use crate::auth::auth_routes::auth_router;
 
 mod auth_repo;
+pub mod auth_routes;
 
-pub(super) fn router() -> OpenApiRouter {
-    OpenApiRouter::new().routes(routes!(forgot_password))
-}
-
-#[derive(ToSchema, Deserialize)]
-struct ForgotPasswordRequest {
-    email: String,
-}
-
-#[utoipa::path(
-    post, path = "/forgot_password",
-    responses(
-        (status = OK, body = String),
-        (status = INTERNAL_SERVER_ERROR, body = String),
-        (status = UNAUTHORIZED, body = String)
-    ),
-    request_body(content = ForgotPasswordRequest, description = "Forgot password request", content_type = "application/json")
-)]
-async fn forgot_password(
-    Extension(pool): Extension<SqlitePool>,
-    axum::extract::Json(payload): axum::extract::Json<ForgotPasswordRequest>,
-) -> Result<Response<axum::body::Body>, Response<axum::body::Body>> {
-    println!(
-        "Handling forgot password request for email {}",
-        payload.email.clone()
-    );
-
-    let repo = AuthRepo::new(&pool);
-
-    let _user = repo
-        .user_for_email(payload.email.clone())
-        .await
-        .map_err(repo_error_handler)?;
-
-    // Here you would implement the logic for handling the forgot password request
-    // For now, we will just return a placeholder response
-    Ok((StatusCode::INTERNAL_SERVER_ERROR, ()).into_response())
-}
-
-fn repo_error_handler(error: AuthRepoError) -> Response<axum::body::Body> {
-    let result = match error {
-        crate::auth::auth_repo::AuthRepoError::UserNotFound => (StatusCode::UNAUTHORIZED, ()),
-        crate::auth::auth_repo::AuthRepoError::DatabaseError => {
-            (StatusCode::INTERNAL_SERVER_ERROR, ())
-        }
-    };
-    result.into_response()
+pub fn router() -> utoipa_axum::router::OpenApiRouter {
+    auth_router()
 }
