4ab2fcbc4d7b2f67a50edc7f74394e48d6e95720
Teq <teq.etsmith@protonmail.com>
2025-07-26 17:59:05 +1000

    add slug validation

--- frontend/src/components/CollectiveForm.tsx
+++ frontend/src/components/CollectiveForm.tsx
@@ -16,6 +16,7 @@ export default function CollectiveForm({ collective, onSubmit }: CollectiveFormP
     validate: {
       name: (value) => (value ? null : "Name is required"),
       noun_name: (value) => (value ? null : "Noun name is required"),
+      slug: (value) => (!value || /^[a-z0-9\-]+$/.test(value) ? null : "Slug may only inlcude lowercase letters, numbers, and hyphens"),
       links: linksValidator,
     },
   });
@@ -26,6 +27,7 @@ export default function CollectiveForm({ collective, onSubmit }: CollectiveFormP
         <Stack gap="md">
           <TextInput label="Name" key="name" {...form.getInputProps("name")} />
           <TextInput label="Noun name" key="noun_name" {...form.getInputProps("noun_name")} />
+          <TextInput label="Slug" key="slug" {...form.getInputProps("slug")} />
           <Textarea label="Description" rows={5} placeholder="Crew Description" key="description" {...form.getInputProps("description")} />
           <LinksInput label="Links" placeholder="Add a link" key="links" {...form.getInputProps("links")} />
         </Stack>
