27a1efea4cdfa695c63c1c403f59cad08c7afc1a
Teq <teq.etsmith@protonmail.com>
2025-07-27 18:21:57 +1000

    renamed eoi to entry_pathways

--- backend/src/eoi/repo.rs
+++ backend/src/eoi/repo.rs
@@ -4,7 +4,7 @@ use crate::shared::entities::{CollectiveId, Eoi};
 
 pub async fn create_eoi(record: Eoi, pool: &SqlitePool) -> Result<(), sqlx::Error> {
     sqlx::query!(
-        "INSERT INTO eoi (collective_id, name, email, interest, context, referral, conflict_experience, participant_connections) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+        "INSERT INTO entry_pathways (collective_id, name, email, interest, context, referral, conflict_experience, participant_connections) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
         record.collective_id,
         record.name,
         record.email,
@@ -25,7 +25,7 @@ pub async fn find_all_eois_for_collective(
 ) -> Result<Vec<Eoi>, sqlx::Error> {
     let eois = sqlx::query_as!(
         Eoi,
-        "SELECT id, collective_id, name, email, interest, context, referral, conflict_experience, participant_connections FROM eoi WHERE collective_id = ?",
+        "SELECT id, collective_id, name, email, interest, context, referral, conflict_experience, participant_connections FROM entry_pathways WHERE collective_id = ?",
         collective_id.id
     )
     .fetch_all(pool)
