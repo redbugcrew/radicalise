f3b4a847f31ffbcc9b802f68b22f1bab9a4cf488
jade <jade@hopepunk.me>
2025-09-24 17:44:45 +1000

    started event template form

--- frontend/src/contexts/events/pages/event_templates/NewEventTemplate.tsx
+++ frontend/src/contexts/events/pages/event_templates/NewEventTemplate.tsx
@@ -1,10 +1,12 @@
 import { Container, Title, Stack } from "@mantine/core";
+import EventTemplateForm from "../../components/event_templates/EventTemplateForm";
 
 export default function NewEventTemplate() {
   return (
     <Container>
       <Stack mb="md">
         <Title order={1}>New Event Template</Title>
+        <EventTemplateForm onSubmit={(data) => console.log("Submitted data:", data)} />
       </Stack>
     </Container>
   );
