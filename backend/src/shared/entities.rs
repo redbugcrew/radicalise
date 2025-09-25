6eea15b85641cc0de43d8ef0db8ea8044c3290b2
jade <jade@hopepunk.me>
2025-09-25 14:54:16 +1000

    can edit event template, still no event handler

--- backend/src/shared/entities.rs
+++ backend/src/shared/entities.rs
@@ -289,7 +289,7 @@ pub struct ExpressionOfInterest {
     pub participant_connections: Option<String>,
 }
 
-#[derive(Deserialize, Serialize, ToSchema, Debug)]
+#[derive(Deserialize, Serialize, ToSchema, Debug, Clone)]
 pub struct EventTemplate {
     pub id: i64,
     pub name: String,
