c200a0bf7dd1114ceae01c49c122b52e1f0e0f5a
Teq <teq.etsmith@protonmail.com>
2025-12-27 18:54:18 +1100

    connect database

--- backend/src/calander_event_attendances/mod.rs
+++ backend/src/calander_event_attendances/mod.rs
@@ -1,14 +1,14 @@
 pub mod repo;
 
 use axum::{
-    response::IntoResponse,
-    http::StatusCode,
+    Extension, http::StatusCode, response::IntoResponse
 };
 use serde::Deserialize;
+use sqlx::SqlitePool;
 use utoipa::ToSchema;
 use utoipa_axum::{router::OpenApiRouter, routes};
 
-use crate::auth::auth_backend::AuthSession;
+use crate::{auth::auth_backend::AuthSession, calander_event_attendances::{self, repo::upsert_event_attendance}};
 
 pub fn router() -> OpenApiRouter {
     OpenApiRouter::new()
@@ -33,10 +33,19 @@ pub struct CreateAttendanceRequest {
 
 async fn create_calendar_event_attendance(
     auth_session: AuthSession,
+    Extension(pool): Extension<SqlitePool>,
     axum::extract::Json(input): axum::extract::Json<CreateAttendanceRequest>,
 ) ->impl IntoResponse {
     let user_id = auth_session.user.unwrap().id;
-    println!("intention: {}, calendar_event_id:{}, user_id:{}", input.intention, input.calendar_event_id, user_id);   
-    (StatusCode::OK, ())
+    println!("updating calender event attendance with: intention: {}, calendar_event_id:{}, user_id:{}", input.intention, input.calendar_event_id, user_id);  
+    let result = upsert_event_attendance(input.calendar_event_id, input.intention, user_id, &pool).await;
+    match result {
+        Ok(_) => {
+            (StatusCode::OK, ())
+        }
+        Err(e) => {
+            println!("error updating calender event attendance: {}", e);
+            (StatusCode::INTERNAL_SERVER_ERROR, ())
+        }
+    }
 }
-
