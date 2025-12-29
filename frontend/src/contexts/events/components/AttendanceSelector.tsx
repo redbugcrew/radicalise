c65160992dc515bb079ebeb5f26a47c3b0d0a114
jade <jade@hopepunk.me>
2025-12-29 17:53:28 +1100

    clean up case

--- frontend/src/contexts/events/components/AttendanceSelector.tsx
+++ frontend/src/contexts/events/components/AttendanceSelector.tsx
@@ -7,6 +7,10 @@ interface AttendanceSelectorProps {
   onChange?: (intention: AttendanceIntention | null) => Promise<void>;
 }
 
+const toTitleCase = (str: string) => {
+  return str.replace(/([A-Z])/g, " $1").trim();
+};
+
 export default function AttendanceSelector({ intention, onChange }: AttendanceSelectorProps) {
   const possibleIntentions = [AttendanceIntention.Going, AttendanceIntention.Uncertain, AttendanceIntention.NotGoing];
   const [loading, setLoading] = useState(false);
@@ -20,5 +24,13 @@ export default function AttendanceSelector({ intention, onChange }: AttendanceSe
     onChange(intention).finally(() => setLoading(false));
   };
 
-  return <SegmentedControl onChange={onSelect} disabled={onChange && loading} data={possibleIntentions} value={intention ?? (null as any)} color="blue" />;
+  return (
+    <SegmentedControl
+      onChange={onSelect}
+      disabled={onChange && loading}
+      data={possibleIntentions.map((intention) => ({ label: toTitleCase(intention), value: intention }))}
+      value={intention ?? (null as any)}
+      color="blue"
+    />
+  );
 }
