91a4e2906acb1df2d21ae63c03014ba6b561818e
jade <jade@hopepunk.me>
2025-12-30 13:11:29 +1100

    send api request

--- frontend/src/contexts/events/components/EventsList.tsx
+++ frontend/src/contexts/events/components/EventsList.tsx
@@ -1,14 +1,24 @@
 import { Stack } from "@mantine/core";
 import type { AttendanceIntention, CalendarEvent } from "../../../api/Api";
 import EventCard from "./EventCard/EventCard";
+import { getApi } from "../../../api";
 
 interface EventsListProps {
   events: CalendarEvent[];
 }
 
 export default function EventsList({ events }: EventsListProps) {
-  const onIntentionChange = async (eventId: number, intention: AttendanceIntention | null) => {
+  const onIntentionChange = async (eventId: number, intention: AttendanceIntention | null): Promise<void> => {
     console.log(`Event ID: ${eventId}, New Intention: ${intention}`);
+
+    getApi()
+      .api.createCalendarEventAttendance({
+        calendar_event_id: eventId,
+        intention: intention,
+      })
+      .catch((error) => {
+        console.error("Failed to update attendance intention:", error);
+      });
   };
 
   return (
