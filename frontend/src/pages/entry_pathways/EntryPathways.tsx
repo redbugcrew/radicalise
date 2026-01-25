3577958744432413cd8722d734b3c49bf8756429
jade <jade@hopepunk.me>
2026-01-25 19:02:37 +1100

    provide link to EOI

--- frontend/src/pages/entry_pathways/EntryPathways.tsx
+++ frontend/src/pages/entry_pathways/EntryPathways.tsx
@@ -1,15 +1,25 @@
-import { Container, Stack, Title } from "@mantine/core";
+import { Container, Stack, Title, Text } from "@mantine/core";
 import { useAppSelector } from "../../store";
-import { EntryPathwaysTable } from "../../components";
+import { Anchor, EntryPathwaysTable } from "../../components";
 
 export default function EntryPathways() {
   const entryPathways = useAppSelector((state) => state.entryPathways);
+  const collectiveSlug = useAppSelector((state) => state.collective?.slug);
 
   return (
     <Container>
       <Stack gap="lg">
-        <Title order={1}>Entry Pathways</Title>
-        <EntryPathwaysTable entryPathways={entryPathways} />
+        <Stack gap="lg">
+          <Title order={1}>Entry Pathways</Title>
+          <EntryPathwaysTable entryPathways={entryPathways} />
+        </Stack>
+        <Stack gap={0}>
+          {collectiveSlug && (
+            <Text c="dimmed">
+              These are created using the <Anchor href={`/collective/${collectiveSlug}/interest`}>expression of interest form</Anchor>.
+            </Text>
+          )}
+        </Stack>
       </Stack>
     </Container>
   );
