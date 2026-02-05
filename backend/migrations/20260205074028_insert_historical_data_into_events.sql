1f0c73d6cd97a5aee28a43bcc843c20132d35511
Teq <teq.etsmith@protonmail.com>
2026-02-05 20:28:08 +1100

    migration for bulk insert of historical data to events and event templates

--- backend/migrations/20260205074028_insert_historical_data_into_events.sql
+++
@@ -1,4 +0,0 @@
--- Add migration script here
-INSERT INTO calendar_events
-(id, event_template_id, name, start_at, end_at, collective_id)
-VALUES(2, 1, 'Assembly 2', '2024-09-22 13:00:00', '2024-09-22 16:00:00', 1);
