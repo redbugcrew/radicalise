03d7af9749c834245375da83ecd61e6449e3efd3
Teq <teq.etsmith@protonmail.com>
2026-02-05 19:51:48 +1100

    update test insert migration

--- backend/migrations/20260205074028_insert_historical_data_into_events.sql
+++ backend/migrations/20260205074028_insert_historical_data_into_events.sql
@@ -1,4 +1,4 @@
 -- Add migration script here
-INSERT INTO calendar_events (event_template_id, name, start_at, end_at, collective_id)
-VALUES
-(1,'Assembly 2', 2024-09-22 13:00:00, 2024-09-22 16:00:00, 1)
+INSERT INTO calendar_events
+(id, event_template_id, name, start_at, end_at, collective_id)
+VALUES(2, 1, 'Assembly 2', '2024-09-22 13:00:00', '2024-09-22 16:00:00', 1);
