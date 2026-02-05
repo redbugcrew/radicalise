26098897d6e9ccf018a90289eee18cbd11cbe309
Teq <teq.etsmith@protonmail.com>
2026-02-05 20:40:14 +1100

    migration files edit

---
+++ backend/migrations/20260205085647_insert_historical_data_into_event_templates.sql
@@ -0,0 +1,13 @@
+-- Add migration script here
+INSERT INTO event_templates
+(id, name, collective_id)
+VALUES
+(1, 'Assembly', 1);
+
+INSERT INTO event_templates
+(id, name, collective_id)
+VALUES
+(2, 'Retreat', 1);
+
+
+
