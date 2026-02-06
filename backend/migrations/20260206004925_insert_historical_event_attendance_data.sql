858143fc8ad0fd8771ed05c2836bd4fd9ddee0fc
Teq <teq.etsmith@protonmail.com>
2026-02-06 13:00:28 +1100

    Write insert statements for data from Assemblies 1 to 8

---
+++ backend/migrations/20260206004925_insert_historical_event_attendance_data.sql
@@ -0,0 +1,168 @@
+-- Add migration script here
+
+-- Assembly 1 No Records
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(1, 1, 1, Null, Null);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(2, 2, 1, Null, Null);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(3, 3, 1, Null, Null);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(1, 4, 1, 'Going', NULL);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(5, 5, 1, Null, Null);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(6, 6, 1, Null, Null);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(7, 7, 1, Null, Null);
+
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(8, 8, 1, Null, Null);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(9, 14, 1, Null, Null);
+
+
+-- Assembly 2 No Records
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(10, 1, 2, Null, Null);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(11, 2, 2, Null, Null);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(12, 3, 2, Null, Null);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(13, 4, 2, Null, Null);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES (14, 5, 2, Null, Null);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES (15, 6, 2, Null, Null);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES (16, 7, 2, Null, Null);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES (17, 8, 2, Null, Null);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES (18, 14, 2, Null, Null);
+
+-- Assembly 3
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(19, 1, 3, 'Going', TRUE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(20, 2, 3, 'Going', TRUE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(21, 3, 3, 'Going', TRUE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(22, 4, 3, 'Going', TRUE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(23, 5, 3, 'NotGoing', FALSE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(24, 6, 3, 'Going', TRUE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(25, 7, 3, 'NotGoing', FALSE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(26, 8, 3, 'Going', TRUE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(27, 13, 3, 'Going', TRUE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(28, 14, 3, 'Going', TRUE);
+
+-- Assembly 4 - No records
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(29, 1, 4, Null, Null);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(30, 2, 4, Null, Null);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(31, 3, 4, Null, Null);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(32, 4, 4, Null, Null);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(33, 5, 4, Null, Null);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(34, 6, 4, Null, Null);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(35, 7, 4, Null, Null);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(36, 8, 4, Null, Null);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(37, 13, 4, Null, Null);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(38, 14, 4, Null, Null);
+
+-- Assembly 5 
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(39, 1, 5, 'Going', TRUE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(40, 2, 5, 'Going', TRUE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(41, 3, 5, 'NotGoing', FALSE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(42, 4, 5, 'Going', TRUE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(43, 5, 5, 'Uncertain', FALSE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(44, 7, 5, 'NotGoing', FALSE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(45, 8, 5, 'Going', TRUE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(46, 9, 5, 'Going', TRUE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(47, 13, 5, 'Going', TRUE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(47, 14, 5, 'Going', TRUE);
+
+-- Assembly 6
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(49, 1, 6, 'NotGoing', FALSE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(50, 2, 6, 'Going', TRUE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(51, 3, 6, 'Going', TRUE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(52, 4, 6, 'Going', TRUE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(53, 5, 6, 'NotGoing', FALSE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(54, 7, 6, 'NotGoing', FALSE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(55, 8, 6, 'Going', TRUE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(56, 9, 6, 'NotGoing', FALSE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(57, 13, 6, 'Going', TRUE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(58, 14, 6, 'NotGoing', FALSE);
+
+-- Assembly 7 
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(59, 1, 7, 'NotGoing', FALSE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(60, 2, 7, 'Going', TRUE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(61, 3, 7, 'Uncertain', FALSE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(62, 4, 7, 'Going', TRUE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(63, 5, 7, 'Uncertain', FALSE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(64, 7, 7, 'Uncertain', FALSE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(65, 8, 7, 'NotGoing', FALSE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(66, 9, 7, 'Going', TRUE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(67, 10, 7, 'Going', TRUE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(68, 13, 7, 'NotGoing', FALSE);
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES(69, 14, 7, 'NotGoing', FALSE);
+
+-- Assembly 8
+
+
+-- Assembly 9
+-- Assembly 10 
+
+INSERT INTO calendar_event_attendances (id, person_id, calendar_event_id, intention, actual) VALUES( , 4, 10, 'NotGoing', NULL);
+
+-- Assembly 11
+-- Assembly 12
+-- Assembly 13
+-- Assembly 14
+-- Assembly 15
+-- Assembly 16
+-- Retreat 1
