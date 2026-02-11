b55aacc28a5f78d131c91119dccc37fda4c49388
jade <jade@hopepunk.me>
2026-02-11 16:01:24 +1100

    return ical

--- backend/src/calendar_events/ical.rs
+++ backend/src/calendar_events/ical.rs
@@ -1,43 +1,42 @@
-use chrono::{Duration, NaiveDate, NaiveTime, Utc};
-use icalendar::{Calendar, CalendarDateTime, Class, Component, Event, EventLike, Property};
+use chrono::NaiveDateTime;
+use icalendar::{Calendar, Component, Event, EventLike};
 
-pub fn get_ical_string() -> String {
-    let my_calendar = Calendar::new()
-        .name("example calendar")
-        .push(
-            // add an event
-            Event::new()
-                .summary("test event")
-                .description("here I have something really important to do")
-                .starts(Utc::now())
-                .class(Class::Confidential)
-                .ends(Utc::now() + Duration::days(1))
-                .append_property(
-                    Property::new("TEST", "FOOBAR")
-                        .add_parameter("IMPORTANCE", "very")
-                        .add_parameter("DUE", "tomorrow"),
-                ),
-        )
-        .push(
-            // add an all-day event
-            Event::new()
-                .all_day(NaiveDate::from_ymd_opt(2016, 3, 15).unwrap())
-                .summary("My Birthday")
-                .description("Hey, I'm gonna have a party\nBYOB: Bring your own beer.\nHendrik"),
-        )
-        .push(
-            // event with utc timezone
-            Event::new()
-                .starts(CalendarDateTime::from(
-                    NaiveDate::from_ymd_opt(2024, 10, 24)
-                        .unwrap()
-                        .and_time(NaiveTime::from_hms_opt(20, 10, 00).unwrap())
-                        .and_utc(),
-                ))
-                .summary("Birthday Party")
-                .description("I'm gonna have a party\nBYOB: Bring your own beer.\nHendrik"),
-        )
-        .done();
+use crate::shared::entities::CalendarEvent;
 
-    my_calendar.to_string()
+pub fn get_ical_string(name: String, events: Vec<CalendarEvent>) -> String {
+    let mut my_calendar = Calendar::new();
+
+    let builder = my_calendar.name(&name);
+
+    events.into_iter().for_each(|event| {
+        let mut cal_event = Event::new();
+        let start_at = parse_naive_date_time(&event.start_at);
+        let end_at = parse_optional_naive_date_time(&event.end_at);
+
+        cal_event
+            .summary(&event.name)
+            .description("")
+            .starts(start_at);
+
+        if let Some(end_at) = &end_at {
+            cal_event.ends(*end_at);
+        }
+
+        builder.push(cal_event);
+    });
+
+    builder.done().to_string()
+}
+
+fn parse_optional_naive_date_time(input: &Option<String>) -> Option<NaiveDateTime> {
+    input.as_ref().map(|s| parse_naive_date_time(s))
+}
+
+fn parse_naive_date_time(input: &str) -> NaiveDateTime {
+    NaiveDateTime::parse_from_str(input, "%Y-%m-%d %H:%M:%S")
+        .map_err(|err| {
+            eprintln!("Failed to parse date time '{}': {}", input, err);
+            err
+        })
+        .unwrap()
 }
