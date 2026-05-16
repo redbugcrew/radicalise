e3d98fc01f7b82a0261cd803517a3f3fe645f7d1
jade <jade@hopepunk.me>
2026-05-16 10:45:11 +1000

    signup works and directs you to login

--- frontend/src/contexts/auth/pages/SignUp.tsx
+++ frontend/src/contexts/auth/pages/SignUp.tsx
@@ -1,14 +1,25 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import { notifications } from "@mantine/notifications";
 import { getApi } from "../../../api";
 import { actionFailure, actionSuccess, type ActionPromiseResult } from "../../../components/ActionResult";
 import AuthLayout from "../components/AuthLayout";
 import LoginForm from "../components/LoginForm";
 
 export default function SignUp() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
   const onSubmit = (values: any): Promise<ActionPromiseResult> => {
     return getApi()
       .api.signUp(values)
       .then((response) => {
         console.log("Sign up successful:", response);
+        notifications.show({
+          title: "Account created",
+          message: "User created successfully.",
+          color: "green",
+        });
+        navigate(`../login${location.search}`);
         return actionSuccess();
       })
       .catch(actionFailure);
