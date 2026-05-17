78adbf3223e2b34d76672a39289800ac699fcb5d
jade <jade@hopepunk.me>
2026-05-17 10:37:52 +1000

    delete unused column

--- backend/migrations/20260516093216_capacity_planning_privacy_is_per_circle.sql
+++ backend/migrations/20260516093216_capacity_planning_privacy_is_per_circle.sql
@@ -5,3 +5,5 @@ ALTER TABLE circle_involvements ADD COLUMN capacity_planning_visibility_circle_i
 UPDATE circle_involvements
 SET capacity_planning_visibility_circle_id = 1
 WHERE private_capacity_planning = FALSE;
+
+ALTER TABLE circle_involvements DROP COLUMN private_capacity_planning;
