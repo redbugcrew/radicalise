626095076a46cbf832e06a6678f92f0c41e199bd
jade <jade@hopepunk.me>
2026-07-02 22:02:45 +1000

    connects up the algorithm

--- backend/src/peer_roles/algorithms/mod.rs
+++ backend/src/peer_roles/algorithms/mod.rs
@@ -1,5 +1,7 @@
 mod random_pairs;
 
+pub use random_pairs::random_pairs;
+
 fn remove_person<PeerId>(person: &PeerId, people: &mut Vec<PeerId>)
 where
     PeerId: Eq,
--- backend/src/peer_roles/mod.rs
+++ backend/src/peer_roles/mod.rs
@@ -1,9 +1,13 @@
+use rand::rng;
 use sqlx::SqlitePool;
 
 use crate::{
     intervals::repo::mark_peer_roles_processed,
+    my_project::involvements_repo::find_all_circle_involvements,
     peer_roles::repo::find_all_peer_roles,
-    shared::entities::{Interval, PeerRole, ProjectId},
+    shared::entities::{
+        CircleId, Interval, IntervalId, PeerRole, PeerRoleDistributionType, ProjectId,
+    },
 };
 
 mod algorithms;
@@ -33,5 +37,36 @@ async fn assign_interval_peer_role(
     peer_role: &PeerRole,
     pool: &SqlitePool,
 ) -> Result<(), sqlx::Error> {
+    let involvements = find_all_circle_involvements(
+        ProjectId::new(peer_role.project_id),
+        CircleId::new(peer_role.circle_id),
+        IntervalId::new(interval.id),
+        pool,
+    )
+    .await?;
+
+    let people: Vec<i64> = involvements.into_iter().map(|i| i.person_id).collect();
+
+    println!(
+        "Assigning peer role for people ids '{:?}' in interval {} ({} people)",
+        people,
+        interval.id,
+        people.len()
+    );
+
+    let results = match peer_role.distribution_type {
+        PeerRoleDistributionType::RandomPairs => {
+            let mut rng = rng();
+            algorithms::random_pairs(people, &mut rng)
+        }
+    };
+
+    println!(
+        "Peer role '{}' (interval {}): {}",
+        peer_role.name,
+        interval.id,
+        results.to_string()
+    );
+
     Ok(())
 }
