d5dc09814caee0ab90b63e451d1fead28e34a311
jade <jade@hopepunk.me>
2026-07-02 19:30:00 +1000

    set interval id in migration

--- backend/migrations/20260622050905_add_current_interval_to_project.sql
+++ backend/migrations/20260622050905_add_current_interval_to_project.sql
@@ -1,2 +1,3 @@
 -- Add migration script here
 ALTER TABLE projects ADD COLUMN current_interval_id INTEGER REFERENCES intervals(id);
+UPDATE projects SET current_interval_id = (SELECT id FROM intervals WHERE intervals.project_id = projects.id ORDER BY id DESC LIMIT 1);
