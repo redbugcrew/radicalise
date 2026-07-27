24f2144506241ae2aa3a107f64d56e98021e3dd8
e t smith <teq.etsmith@protonmail.com>
2026-07-27 16:28:10 +1000

    passed in data from the peer roles page

--- frontend/src/contexts/peer_roles/components/PeerRolesTable.tsx
+++ frontend/src/contexts/peer_roles/components/PeerRolesTable.tsx
@@ -1,64 +1,24 @@
 import { Stack, Table, Text } from "@mantine/core";
-import { Anchor } from "../../../components";
 
-interface PeerRolesTableProps {}
+interface PeerRolesTableProps {
+  peerRoles: PeerRole[];
+}
 
-//function PeerRoleTableRow({
-//  template,
-//}: {
-//  template: PeerRoleTemplate;
-//}) {
-//  if (!template) return null;
-//
-//  return (
-//    <Table.Tr key={template.id}>
-//      <Table.Td>{template.id}</Table.Td>
-//      <Table.Td>
-//        <Stack gap={0}>
-//          <Text>{template.name}</Text>
-//          <Text c="dimmed">{template.summary}</Text>
-//        </Stack>
-//      </Table.Td>
-//      <Table.Td>
-//        <Anchor href={`${template.id}/edit`}>Edit</Anchor>
-//      </Table.Td>
-//    </Table.Tr>
-//  );
-//}
+interface PeerRole {
+  role: number;
+  name: string;
+  summary: string;
+}
 
-const dummypeers = [
-  {
-    Role: 1,
-    Name: "Buddy",
-    Summary:
-      "Participation Buddies support each-other to communicate and meet our participation intentions in the project",
-    Action: "edit",
-  },
-  {
-    Role: 2,
-    Name: "Care Supporter",
-    Summary:
-      "The role of Care Supporters is to offer a holding space to practice expressing our feelings and reflecting on our experiences without exposing ourselves to either judgement or rescue.",
-    Action: "edit",
-  },
-  {
-    Role: 3,
-    Name: "Accountability Supporter",
-    Summary:
-      "The role of Accountability supporters is to offer gentle challenges to practice reflecting on alternative perspectives and holding ourselves accountable for how our actions impact others (without policing each others' behaviour).",
-    Action: "edit",
-  },
-];
-
-export default function PeerRolesTable({}: PeerRolesTableProps) {
-  const rows = dummypeers.map((dummypeers) => (
-    <Table.Tr key={dummypeers.Name}>
-      <Table.Td>{dummypeers.Role}</Table.Td>
+export default function PeerRolesTable({ peerRoles }: PeerRolesTableProps) {
+  const rows = peerRoles.map((role) => (
+    <Table.Tr key={role.name}>
+      <Table.Td>{role.role}</Table.Td>
       <Stack gap={1}>
-        <Text>{dummypeers.Name}</Text>
-        <Text c="dimmed">{dummypeers.Summary}</Text>
+        <Text>{role.name}</Text>
+        <Text c="dimmed">{role.summary}</Text>
       </Stack>
-      <Table.Td>{dummypeers.Action}</Table.Td>
+      <Table.Td>edit</Table.Td>
     </Table.Tr>
   ));
 
--- frontend/src/contexts/peer_roles/pages/PeerRoles.tsx
+++ frontend/src/contexts/peer_roles/pages/PeerRoles.tsx
@@ -4,13 +4,26 @@ import { Anchor } from "@mantine/core";
 import PeerRolesTable from "../components/PeerRolesTable";
 
 export default function EventTemplates() {
-  //const PeerRoleTemplatesTable = [
-  //  { position: 6, mass: 12.011, symbol: "C", name: "Carbon" },
-  //  { position: 7, mass: 14.007, symbol: "N", name: "Nitrogen" },
-  //  { position: 39, mass: 88.906, symbol: "Y", name: "Yttrium" },
-  //  { position: 56, mass: 137.33, symbol: "Ba", name: "Barium" },
-  //  { position: 58, mass: 140.12, symbol: "Ce", name: "Cerium" },
-  //];
+  const dummyPeers = [
+    {
+      role: 1,
+      name: "Buddy",
+      summary:
+        "Participation Buddies support each-other to communicate and meet our participation intentions in the project",
+    },
+    {
+      role: 2,
+      name: "Care Supporter",
+      summary:
+        "The role of Care Supporters is to offer a holding space to practice expressing our feelings and reflecting on our experiences without exposing ourselves to either judgement or rescue.",
+    },
+    {
+      role: 3,
+      name: "Accountability Supporter",
+      summary:
+        "The role of Accountability supporters is to offer gentle challenges to practice reflecting on alternative perspectives and holding ourselves accountable for how our actions impact others (without policing each others' behaviour).",
+    },
+  ];
 
   return (
     <Stack>
@@ -22,7 +35,7 @@ export default function EventTemplates() {
           </ActionIcon>
         </Anchor>
       </Group>
-      <PeerRolesTable />
+      <PeerRolesTable peerRoles={dummyPeers} />
     </Stack>
   );
 }
