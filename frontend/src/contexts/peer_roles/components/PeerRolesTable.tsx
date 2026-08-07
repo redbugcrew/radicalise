812e08852f5ce444e51a2d73227ceca4cff160f7
e t smith <teq.etsmith@protonmail.com>
2026-08-07 12:03:10 +1000

    add summary and distribution type to display table

--- frontend/src/contexts/peer_roles/components/PeerRolesTable.tsx
+++ frontend/src/contexts/peer_roles/components/PeerRolesTable.tsx
@@ -9,7 +9,8 @@ export default function PeerRolesTable({ peerRoles }: PeerRolesTableProps) {
   const rows = peerRoles.map((role) => (
     <Table.Tr key={role.name}>
       <Table.Td>{role.name}</Table.Td>
-      {/*<Table.Td>{role.summary}</Table.Td>*/}
+      <Table.Td>{role.summary}</Table.Td>
+      <Table.Td>{role.distribution_type}</Table.Td>
       <Table.Td>edit</Table.Td>
     </Table.Tr>
   ));
@@ -19,7 +20,8 @@ export default function PeerRolesTable({ peerRoles }: PeerRolesTableProps) {
       <Table.Thead>
         <Table.Tr>
           <Table.Th>Role</Table.Th>
-          {/*<Table.Th>Summary</Table.Th>*/}
+          <Table.Th>Summary</Table.Th>
+          <Table.Th>Distribution</Table.Th>
           <Table.Th>Actions</Table.Th>
         </Table.Tr>
       </Table.Thead>
